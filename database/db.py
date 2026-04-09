29d09c944a797774383c8b035228af5ac178c603
Ruhul Amin Sharif <sharif.cse.ras@gmail.com>
2026-04-09 18:16:24 +0600

    feat: add get_user_by_email function to retrieve user by email

--- database/db.py
+++ database/db.py
@@ -50,6 +50,14 @@ def create_user(name, email, password):
   conn.close()
   return user_id
 
+def get_user_by_email(email):
+  conn = get_db()
+  user = conn.execute(
+    "SELECT * FROM users WHERE email = ?", (email,)
+  ).fetchone()
+  conn.close()
+  return user
+
 def seed_db():
   conn = get_db()
 
